67536ed842d56a355727619cc61d39716d480736
leomiranda <leonardomir@gmail.com>
2024-03-07 21:24:10 -0400

    feat: header

---
+++ app/components/Logo.tsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+	return (
+		<p className="uppercase tracking-tighter">
+			<span className="text-gray-500">Form</span>
+			<span className="font-semibold text-gray-700 dark:text-gray-200">
+				Builder
+			</span>
+		</p>
+	);
+}
--- app/root.tsx
+++ app/root.tsx
@@ -7,25 +7,43 @@ import {
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	useLoaderData,
 } from '@remix-run/react';
 import { rootAuthLoader } from '@clerk/remix/ssr.server';
 import { ClerkApp, ClerkErrorBoundary } from '@clerk/remix';
 import styles from './tailwind.css';
+import clsx from 'clsx';
+import {
+	PreventFlashOnWrongTheme,
+	ThemeProvider,
+	useTheme,
+} from 'remix-themes';
+import { themeSessionResolver } from './sessions.server';
 
 export const links: LinksFunction = () => [
 	...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 	{ rel: 'stylesheet', href: styles },
 ];
 
-export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+export const loader: LoaderFunction = async (args) => {
+	return rootAuthLoader(args, async ({ request }) => {
+		const { getTheme } = await themeSessionResolver(request);
+		return {
+			theme: getTheme(),
+		};
+	});
+};
 
 function App() {
+	const data = useLoaderData<typeof loader>();
+	const [theme] = useTheme();
 	return (
-		<html lang="en">
+		<html lang="en" className={clsx(theme)}>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
+				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
 				<Links />
 			</head>
 			<body>
@@ -40,4 +58,13 @@ function App() {
 
 export const ErrorBoundary = ClerkErrorBoundary();
 
-export default ClerkApp(App);
+function AppWithProviders() {
+	const data = useLoaderData<typeof loader>();
+	return (
+		<ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+			<App />
+		</ThemeProvider>
+	);
+}
+
+export default ClerkApp(AppWithProviders);
