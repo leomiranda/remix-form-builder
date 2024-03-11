92d5546db4a5ca2f3a65a5b7250234d165c23b04
leomiranda <leonardomir@gmail.com>
2024-03-11 19:47:13 -0400

    feat: add link to logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,10 +1,12 @@
+import { Link } from '@remix-run/react';
+
 export default function Logo() {
 	return (
-		<p className="uppercase tracking-tighter">
+		<Link to="/" className="uppercase tracking-tighter">
 			<span className="text-gray-500">Form</span>
 			<span className="font-semibold text-gray-700 dark:text-gray-200">
 				Builder
 			</span>
-		</p>
+		</Link>
 	);
 }
