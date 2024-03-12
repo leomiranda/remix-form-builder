b564bc31771d6b7f15204660f91021097e21d13b
leomiranda <leonardomir@gmail.com>
2024-03-11 21:01:17 -0400

    feat: get user ssr

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -1,21 +1,28 @@
-import {
-	SignedIn,
-	SignedOut,
-	RedirectToSignIn,
-	UserButton,
-} from '@clerk/remix';
+import { getAuth } from '@clerk/remix/ssr.server';
+import { LoaderFunction, json, redirect } from '@remix-run/node';
+import { createClerkClient } from '@clerk/remix/api.server';
+import { useLoaderData } from '@remix-run/react';
 
-export default function Index() {
+export const loader: LoaderFunction = async (args) => {
+	const { userId } = await getAuth(args);
+
+	if (!userId) {
+		return redirect('/sign-in?redirect_url=' + args.request.url);
+	}
+
+	const user = await createClerkClient({
+		secretKey: process.env.CLERK_SECRET_KEY,
+	}).users.getUser(userId);
+
+	return json({ user });
+};
+
+export default function HomeIndex() {
+	const { user } = useLoaderData<typeof loader>();
+
 	return (
-		<div>
-			<SignedIn>
-				<h1>Index route</h1>
-				<p>You are signed in!</p>
-				<UserButton />
-			</SignedIn>
-			<SignedOut>
-				<RedirectToSignIn />
-			</SignedOut>
+		<div className="container pt-4">
+			<p>Hello {user.firstName}</p>
 		</div>
 	);
 }
